6af7e0783cdb7e20e46312b941d8f35a023e92e1
dambaq <damaciob.98@gmail.com>
2023-10-18 01:52:46 -0600

    add folder modelos

---
+++ modelos/erp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from datetime import datetime
+
+class Type(models.Model):...
+    
+# Create your models here.
+class Employee(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    names = models.CharField(max_length=150, verbose_name='Nombres y Apellidos')
+    dpi = models.CharField(max_length=13, unique= 10, verbose_name='DPI')
+    date = models.DateField(verbose_name='Fecha de nacimiento')
+    telefono = models.CharField(max_length=8, verbose_name='Telefono')
+    user = models.CharField(max_length=10, verbose_name='Nombre de usuario')
+    correo = models.CharField(max_length=30, verbose_name='Correo electronico')
+    contraseña = models.CharField(max_length=25, verbose_name='Contraseña')
+
+    def __str__(self):
+        return self.names
+
+    class Meta:
+        verbose_name = 'alumno'
+        verbose_name_plural = 'alumnos'
+        db_table = 'alumno'
+        ordering = ['id']
